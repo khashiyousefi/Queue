99ca04e7d75aedfd7e0e122d7dcb34d59e4f58e1
kashy <kashy@sfu.ca>
2020-06-27 17:12:42 -0700

    reversing Queue added

--- src/com/SixLink/Main.java
+++ src/com/SixLink/Main.java
@@ -1,8 +1,36 @@
 package com.SixLink;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        queue.remove();
+        reverse(queue);
+        System.out.println(queue);
+    }
+    public static void reverse(Queue<Integer> queue){
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty()){
+            stack.push(queue.remove());
+        }
+        while (!stack.empty()){
+            queue.add(stack.pop());
+        }
+    }
+
+    public static void arrayReverse(Queue<Integer> queue){
+        while(!queue.isEmpty()){
+
+        }
     }
 }
